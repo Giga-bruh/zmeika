abace3f6d8c55d821122ea82aab59663e5640d5a
Михаил <zhand31@mail.ru>
2024-08-01 14:17:21 +0300

    змейка

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import random
+
 import pygame as p
 import kletka
+import baza_zmei
 from nastroiki import *
 class Game:
     def __init__(self,kletki,stolbci,promezhytok):
         self.screen=p.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.spisok_kletok=[]
         self.kletki=kletki
-        self.x=0
-        self.itap2=0
-        self.y=0
-        self.stolbci=stolbci
-        self.promezhytok=promezhytok
+        self.itap2 = 0
+        self.stolbci = stolbci
+        self.promezhytok = promezhytok
+        self.x = 0
+
+        self.y = 0
+        self.sozdanie_kletok()
+        self.random=random.randint(0,len(self.spisok_kletok)-1)
+        self.zmeika_golova=baza_zmei.Baza_zmeya(self,self.spisok_kletok[self.random].pramoygolnik.x+6,self.spisok_kletok[self.random].pramoygolnik.y,"Snake sprite sheet_1.png","Snake sprite sheet_0.png",SCREEN_WIDTH/self.stolbci-self.promezhytok,SCREEN_HEIGHT/self.stolbci-self.promezhytok)
+
+
         self.clock = p.time.Clock()
         self.run()
 
@@ -20,8 +29,12 @@ class Game:
             self.sozdanie_kletok()
             self.otrisovka()
             self.event()
+
+            for kletko in self.spisok_kletok:
+                if kletko.pramoygolnik.colliderect(self.zmeika_golova.pramoygolnik):
+                    self.zmeika_golova.ypravlenie(kletko)
             self.clock.tick(FPS)
-            # self.screen.fill([1,1,1])
+            self.screen.fill([1,1,1])
     def sozdanie_kletok(self):
 
         zakoncheno_ili_net=0
@@ -70,9 +83,12 @@ class Game:
         for event in p.event.get():
             if event.type == pg.QUIT:
                 quit()
+
     def otrisovka(self):
         for kletka in self.spisok_kletok:
             kletka.otrisovka()
+        self.zmeika_golova.otrisovka()
+
         p.display.flip()
 if __name__ == "__main__":
     Game(18,18,10)
--- nastroiki.py
+++ nastroiki.py
@@ -10,3 +10,8 @@ font = pg.font.Font(None, 40)
 
 def text_render(text):
     return font.render(str(text), True, "black")
+
+def load_image(file, width, height):
+    image = pg.image.load(file).convert_alpha()
+    image = pg.transform.scale(image, (width, height))
+    return image
